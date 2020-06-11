5c50105a5aac32d8a62424a9d11dc22909cd4a39
dbpatnodee <dbpatnode@hotmail.com>
2020-06-10 21:39:31 -0700

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,31 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification, :mechanic, :car_owner
 
-  def initialize(make, model, classification)
+  @@all= []
+
+  def initialize(make, model, classification, mechanic, car_owner)
     @make = make
     @model = model
     @classification = classification
+    @car_owner = car_owner
+    @mechanic = mechanic
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+  
+  # `Car.classifications` Get a list of all car classifications
+  def self.classifications
+     self.all.collect {|car|car.classification}
+  end
+
+  # Car.find_mechanics(classification) Get a list of mechanics that have an 
+  # expertise that matches the passed in car classification
+  def self.find_mechanics(classification)
+    array = self.all.select {|car| car.classification == classification}
+    array.collect {|classification| classification.mechanic}
+  end
 end
